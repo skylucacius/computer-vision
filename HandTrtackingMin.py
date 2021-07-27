1e78dc1713dbb42dd8e2ad33162725b7c91054c8
skylucacius <skylucacius@hotmail.com>
2021-07-27 09:01:37 -0300

    básico em rastreamento de mãos

--- HandTrtackingMin.py
+++ HandTrtackingMin.py
@@ -3,18 +3,47 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
-success, img = cap.read()
-print(success)
-print(cap.isOpened())
+# success, img = cap.read()
+# print(success)
+# print(cap.isOpened())
 
-# while True:
-#     sucess, img = cap.read()
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+#   def __init__(self,
+#                static_image_mode=False,
+#                max_num_hands=2,
+#                min_detection_confidence=0.5,
+#                min_tracking_confidence=0.5):
+mpDraw = mp.solutions.drawing_utils
+pTime = 0; cTime = 0
 
-#     print(sucess)
+while True:
+    sucess, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
 
-    # cv2.imshow("Image", img)
-    # cv2. waitKey(1)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id,lm in enumerate(handLms.landmark):
+                # print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                if (id == 0):
+                    cv2.circle(img, (cx,cy), 25, (255,0,255), cv2.FILLED)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    # print(sucess)
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255),3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
 
 
-cap.release()
-cv2.destroyAllWindows()
+# cap.release()
+# cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Usaremos as libs OpenCV-Python e Mediapipe para seguir o curso de 6 horas disponível em:
 https://www.freecodecamp.org/news/advanced-computer-vision-with-python/
+
+Está sendo desenvolvimento uma aplicação de aprendizado a visão computacional com base em identificação e rastreamento de mãos.
