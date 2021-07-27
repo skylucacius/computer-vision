5b61894a933d4b53ae0a605b66c4f32a4fff79b8
skylucacius <skylucacius@hotmail.com>
2021-07-27 09:54:38 -0300

    rastreamento de mãos - concluído

---
+++ MyNewGameHandTracking.py
@@ -0,0 +1,24 @@
+import cv2
+import mediapipe as mp
+import time
+import HandTrackingModule as htm
+
+pTime = 0; cTime = 0
+cap = cv2.VideoCapture(0)
+detector = htm.handDetector()
+
+while True:
+    sucess, img = cap.read()
+    draw = False
+    img = detector.findHands(img, draw)
+    lmList = detector.findPosition(img, draw=draw)
+    if len(lmList) != 0:
+        print(lmList[4])
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255),3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
